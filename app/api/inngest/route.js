e4da082a8f9c83179c19d792e07737db3162dc2f
Pardha Saradhi <seerapupardhu123@gmail.com>
2025-05-30 00:26:39 +0530

    implemented monthly report generation using inngest and gemini ai for insights which triggers on 1st of every month

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlert, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions";
+import { checkBudgetAlert, generateMonthlyReport, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -8,6 +8,7 @@ export const { GET, POST, PUT } = serve({
   functions: [
     checkBudgetAlert,
     triggerRecurringTransactions,
-    processRecurringTransaction
+    processRecurringTransaction,
+    generateMonthlyReport
   ],
 });

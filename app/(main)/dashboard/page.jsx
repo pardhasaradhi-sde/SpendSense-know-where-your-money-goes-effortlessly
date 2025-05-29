8eacbd8e766359eb9cf9a212c2b2fa60aa24b1e4
Pardha Saradhi <seerapupardhu123@gmail.com>
2025-05-30 01:17:32 +0530

    implemented transaction overview and a piechart of transactions of previous month

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import CreateAccountDrawer from '@/components/CreateAccountDrawer'
 import { Card, CardContent } from '@/components/ui/card';
 import { Plus } from 'lucide-react';
-import { getUserAccounts } from '@/actions/dashboard';
+import { getDashboardData, getUserAccounts } from '@/actions/dashboard';
 import {AccountCard} from './_components/accountCard';
 import { getCurrentBudget } from '@/actions/budget';    
-import  BudgetProgress  from './_components/budgetProgress';    
+import  BudgetProgress  from './_components/budgetProgress';   
+import DashboardOverview from './_components/dashboardoverview'; 
 async function DashboardPage() {
     const accounts = await getUserAccounts();
     const defaultAccount = accounts.find((account) => account.isDefault);
@@ -13,9 +14,13 @@ async function DashboardPage() {
     if(defaultAccount){
         budgetData =await getCurrentBudget(defaultAccount.id);
     }  
+    const transactions=await getDashboardData();
     return (
         <div className='space-y-8'>
             {defaultAccount && <BudgetProgress initialBudget={budgetData?.budget}   currentExpenses={budgetData?.currentExpenses||0}  />}
+            <Suspense fallback={"loading overview..."}>
+                <DashboardOverview accounts={accounts} transactions={transactions || []} />
+            </Suspense>
             <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
                 <CreateAccountDrawer>
                     <Card className='hover:shadow-md transition-shadow cursor-pointer border-dashed'>
